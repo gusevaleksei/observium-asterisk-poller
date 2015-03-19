c1c39f096007dbbe790d099fbb92bb463839ba91
Alexey Gusev <gusev.alexey.v@gmail.com>
2015-03-19 22:36:00 +0300

    beta
    few simple graphs
    
    Changes to be committed:
    	modified:   config.add.php
    	modified:   includes/polling/asterisk.inc.php

--- config.add.php
+++ config.add.php
@@ -1,3 +1,52 @@
 <?php
 
+$config['mibs']['ASTERISK-MIB'] = 1;
+$config['mibs']['DIGIUM-MIB'] = 1;
+
+$config['poller_modules']['asterisk'] = 1;
+$config['discovery_modules']['asterisk'] = 1;
+
+$config['graph_types']['device']['asterisk_processed'] = array(
+  'order'     => '1',
+  'section'   => 'asterisk',
+  'descr'     => 'Processed Calls',
+  'file'      => 'asterisk.rrd',
+  'colours'   => 'blues',
+  'unit_text' => 'calls',
+  'num_fmt'   => '8.0',
+  'no_mag'    => TRUE,
+  'nototal' => FALSE,
+  'rra_min' => FALSE,
+  'rra_max' => FALSE,
+  'ds'        => array(
+    'asterisk_processed' => array('label' => 'Processed calls', 'draw' => 'AREA', 'line' => TRUE, 'rra_min' => FALSE, 'rra_max' => FALSE),
+  )
+);
+
+$config['graph_types']['device']['asterisk_channels'] = array(
+  'section'   => 'asterisk',
+  'descr'     => 'Active channels',
+  'file'      => 'asterisk.rrd',
+  'colours'   => 'blues',
+  'unit_text' => 'channels',
+  'num_fmt'   => '8.0',
+  'no_mag'    => TRUE,
+  'ds'        => array(
+    'asterisk_channels' => array('label' => 'Active channels', 'draw' => 'AREASTACK', 'line' => TRUE, 'rra_min' => FALSE, 'rra_max' => FALSE)
+  )
+);
+
+$config['graph_types']['device']['asterisk_bridged'] = array(
+  'section'   => 'asterisk',
+  'descr'     => 'Active conversations',
+  'file'      => 'asterisk.rrd',
+  'colours'   => 'blues',
+  'unit_text' => 'conversations',
+  'num_fmt'   => '8.0',
+  'no_mag'    => TRUE,
+  'ds'        => array(
+    'asterisk_bridged' => array('label' => 'Active conversations', 'draw' => 'AREA', 'line' => TRUE, 'rra_min' => FALSE, 'rra_max' => FALSE)
+  )
+);
+
 
--- includes/polling/asterisk.inc.php
+++ includes/polling/asterisk.inc.php
@@ -1,3 +1,41 @@
 <?php
 
+// file_put_contents('/tmp/asterisk.inc.txt', var_export($device, true));
+
+if ($device['type'] == 'voip')
+{
+    echo("VOIP:\n");
+    include("includes/include-dir-mib.inc.php");
+
+    $asterisk_rrd = "asterisk.rrd";
+
+
+    $asterisk_proc_calls = snmp_walk($device, "astConfigCallsProcessed", "-OUqnv", "ASTERISK-MIB", $mibdir, FALSE) + 0;
+    echo("Asterisk processed calls: " . $asterisk_proc_calls . "\n");
+
+    $ast_channels = snmp_walk($device, "astNumChannels",  "-Oq", "ASTERISK-MIB", $mibdir, FALSE) + 0;
+    $ast_bridged = snmp_walk($device, "astNumChanBridge", "-Oq", "ASTERISK-MIB", $mibdir, FALSE) + 0;
+
+    // echo(" " . snmp_walk($device, "astConfigUpTime", "-Oq", "ASTERISK-MIB", $mibdir) . "\n");
+    // echo(" " . snmp_walk($device, "astChanTypeName", "-Oq", "ASTERISK-MIB", $mibdir) . "\n");
+    // echo(" " . snmp_walk($device, "astChanTypeName", "-Oq", "ASTERISK-MIB", $mibdir) . "\n");
+    // echo(" " . snmp_walk($device, "astChanTypeChannels", "-Oq", "ASTERISK-MIB", $mibdir) . "\n");
+
+    rrdtool_create($device, $asterisk_rrd, " \
+        DS:asterisk_processed:GAUGE:600:0:U \
+        DS:asterisk_channels:GAUGE:600:0:U \
+        DS:asterisk_bridged:GAUGE:600:0:U \
+        ");
+
+    rrdtool_update($device, $asterisk_rrd, array($asterisk_proc_calls, $ast_channels, $ast_bridged));
+
+    $graphs['asterisk_processed'] = TRUE;
+    $graphs['asterisk_channels'] = TRUE;
+    $graphs['asterisk_bridged'] = TRUE;
+
+}
+
+echo(PHP_EOL);
+
+// EOF
 
